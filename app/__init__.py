c8ce6c9cb2daadaa663b68ddf4d9c6871598821b
oldboy465 <167724562+oldboy465@users.noreply.github.com>
2026-04-28 16:23:00 -0300

    fix: reativando criacao de tabelas para o banco em memoria

--- app/__init__.py
+++ app/__init__.py
@@ -130,11 +130,11 @@ def create_app(config_name='default'):
 
     # 7. Inicialização do Banco
     # Apenas garante que as tabelas existem, não apaga dados
-    #with app.app_context():
+    with app.app_context():
         pass
         # LINHAS COMENTADAS PARA EVITAR ERRO DE READ-ONLY NO VERCEL
-        # db.create_all()
-        # create_default_admin()
+        db.create_all()
+        create_default_admin()
 
     return app
 
